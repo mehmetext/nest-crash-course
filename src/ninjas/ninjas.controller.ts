c5c2fa943a1bd1502cf379b5cfd0706502158b79
Mehmet <mehmetext68@gmail.com>
2024-01-30 16:02:27 +0300

    add ninjas service functions

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -10,38 +10,35 @@ import {
 } from '@nestjs/common';
 import { CreateNinjaDto } from './dto/create-ninja.dto';
 import { UpdateNinjaDto } from './dto/update-ninja.dto';
+import { NinjasService } from './ninjas.service';
 
 @Controller('ninjas')
 export class NinjasController {
+  constructor(private readonly ninjasService: NinjasService) {}
+
   @Get()
-  getNinjas(@Query('type') type: string) {
-    return [{ type }];
+  getNinjas(@Query('weapon') weapon?: 'stars' | 'nunchuks' | 'all') {
+    weapon = weapon === 'all' ? undefined : weapon;
+    return this.ninjasService.getNinjas(weapon);
   }
 
   @Get(':id')
   getNinja(@Param('id') id: string) {
-    return {
-      id: id,
-    };
+    return this.ninjasService.getNinja(+id);
   }
 
   @Post()
   addNinja(@Body() createNinjaDto: CreateNinjaDto) {
-    return createNinjaDto;
+    return this.ninjasService.createNinja(createNinjaDto);
   }
 
   @Put(':id')
   updateNinja(@Param('id') id: string, @Body() updateNinjaDto: UpdateNinjaDto) {
-    return {
-      id: id,
-      ...updateNinjaDto,
-    };
+    return this.ninjasService.updateNinja(+id, updateNinjaDto);
   }
 
   @Delete(':id')
   removeNinja(@Param('id') id: string) {
-    return {
-      id: id,
-    };
+    return this.ninjasService.removeNinja(+id);
   }
 }
--- src/ninjas/ninjas.service.ts
+++ src/ninjas/ninjas.service.ts
@@ -1,4 +1,56 @@
 import { Injectable } from '@nestjs/common';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
 
 @Injectable()
-export class NinjasService {}
+export class NinjasService {
+  private ninjas: CreateNinjaDto[] = [
+    { id: 0, name: 'ninjaA', weapon: 'stars' },
+    { id: 1, name: 'ninjaB', weapon: 'nunchuks' },
+  ];
+
+  getNinjas(weapon?: 'stars' | 'nunchuks') {
+    if (weapon) {
+      return this.ninjas.filter((n) => n.weapon === weapon);
+    }
+    return this.ninjas;
+  }
+
+  getNinja(id: number) {
+    const ninja = this.ninjas.find((n) => n.id === id);
+    if (!ninja) {
+      throw new Error('ninja not found');
+    }
+    return ninja;
+  }
+
+  createNinja(createNinjaDto: CreateNinjaDto) {
+    createNinjaDto.id = Date.now();
+    this.ninjas.push(createNinjaDto);
+    return createNinjaDto;
+  }
+
+  updateNinja(id: number, updateNinjaDto: UpdateNinjaDto) {
+    this.ninjas = this.ninjas.map((n) => {
+      if (n.id === id) {
+        n = { ...n, ...updateNinjaDto };
+      }
+
+      return n;
+    });
+
+    return this.getNinja(id);
+  }
+
+  removeNinja(id: number) {
+    let toBeRemoved: CreateNinjaDto;
+
+    this.ninjas = this.ninjas.filter((n) => {
+      if (n.id === id) toBeRemoved = n;
+
+      return n.id !== id;
+    });
+
+    return toBeRemoved;
+  }
+}
