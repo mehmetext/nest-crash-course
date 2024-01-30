16bc6dfb68fb6cac1932628d6e016b3e08b40f1c
Mehmet <mehmetext68@gmail.com>
2024-01-30 16:31:01 +0300

    add belt guard

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -9,11 +9,13 @@ import {
   Post,
   Put,
   Query,
+  UseGuards,
   ValidationPipe,
 } from '@nestjs/common';
 import { CreateNinjaDto } from './dto/create-ninja.dto';
 import { UpdateNinjaDto } from './dto/update-ninja.dto';
 import { NinjasService } from './ninjas.service';
+import { BeltGuard } from 'src/belt/belt.guard';
 
 @Controller('ninjas')
 export class NinjasController {
@@ -35,6 +37,7 @@ export class NinjasController {
   }
 
   @Post()
+  @UseGuards(BeltGuard)
   addNinja(@Body(ValidationPipe) createNinjaDto: CreateNinjaDto) {
     return this.ninjasService.createNinja(createNinjaDto);
   }
