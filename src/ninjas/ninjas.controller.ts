e6bb8c379ef89e249cc1b2511be2db6556e23299
Mehmet <mehmetext68@gmail.com>
2024-01-30 16:36:54 +0300

    add ninja entity

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -16,19 +16,20 @@ import { CreateNinjaDto } from './dto/create-ninja.dto';
 import { UpdateNinjaDto } from './dto/update-ninja.dto';
 import { NinjasService } from './ninjas.service';
 import { BeltGuard } from 'src/belt/belt.guard';
+import { Ninja } from './entities/ninja.entity';
 
 @Controller('ninjas')
 export class NinjasController {
   constructor(private readonly ninjasService: NinjasService) {}
 
   @Get()
-  getNinjas(@Query('weapon') weapon?: 'stars' | 'nunchuks' | 'all') {
+  getNinjas(@Query('weapon') weapon?: 'stars' | 'nunchuks' | 'all'): Ninja[] {
     weapon = weapon === 'all' ? undefined : weapon;
     return this.ninjasService.getNinjas(weapon);
   }
 
   @Get(':id')
-  getNinja(@Param('id', ParseIntPipe) id: number) {
+  getNinja(@Param('id', ParseIntPipe) id: number): Ninja {
     try {
       return this.ninjasService.getNinja(id);
     } catch (err) {
@@ -38,17 +39,20 @@ export class NinjasController {
 
   @Post()
   @UseGuards(BeltGuard)
-  addNinja(@Body(ValidationPipe) createNinjaDto: CreateNinjaDto) {
+  addNinja(@Body(ValidationPipe) createNinjaDto: CreateNinjaDto): Ninja {
     return this.ninjasService.createNinja(createNinjaDto);
   }
 
   @Put(':id')
-  updateNinja(@Param('id') id: string, @Body() updateNinjaDto: UpdateNinjaDto) {
+  updateNinja(
+    @Param('id') id: string,
+    @Body() updateNinjaDto: UpdateNinjaDto,
+  ): Ninja {
     return this.ninjasService.updateNinja(+id, updateNinjaDto);
   }
 
   @Delete(':id')
-  removeNinja(@Param('id') id: string) {
+  removeNinja(@Param('id') id: string): Ninja {
     return this.ninjasService.removeNinja(+id);
   }
 }
--- src/ninjas/ninjas.service.ts
+++ src/ninjas/ninjas.service.ts
@@ -1,22 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { CreateNinjaDto } from './dto/create-ninja.dto';
 import { UpdateNinjaDto } from './dto/update-ninja.dto';
+import { Ninja } from './entities/ninja.entity';
 
 @Injectable()
 export class NinjasService {
-  private ninjas: CreateNinjaDto[] = [
+  private ninjas: Ninja[] = [
     { id: 0, name: 'ninjaA', weapon: 'stars' },
     { id: 1, name: 'ninjaB', weapon: 'nunchuks' },
   ];
 
-  getNinjas(weapon?: 'stars' | 'nunchuks') {
+  getNinjas(weapon?: 'stars' | 'nunchuks'): Ninja[] {
     if (weapon) {
       return this.ninjas.filter((n) => n.weapon === weapon);
     }
     return this.ninjas;
   }
 
-  getNinja(id: number) {
+  getNinja(id: number): Ninja {
     const ninja = this.ninjas.find((n) => n.id === id);
     if (!ninja) {
       throw new Error('ninja not found');
@@ -24,13 +25,13 @@ export class NinjasService {
     return ninja;
   }
 
-  createNinja(createNinjaDto: CreateNinjaDto) {
+  createNinja(createNinjaDto: CreateNinjaDto): Ninja {
     createNinjaDto.id = Date.now();
     this.ninjas.push(createNinjaDto);
     return createNinjaDto;
   }
 
-  updateNinja(id: number, updateNinjaDto: UpdateNinjaDto) {
+  updateNinja(id: number, updateNinjaDto: UpdateNinjaDto): Ninja {
     this.ninjas = this.ninjas.map((n) => {
       if (n.id === id) {
         n = { ...n, ...updateNinjaDto };
@@ -42,8 +43,8 @@ export class NinjasService {
     return this.getNinja(id);
   }
 
-  removeNinja(id: number) {
-    let toBeRemoved: CreateNinjaDto;
+  removeNinja(id: number): Ninja {
+    let toBeRemoved: Ninja;
 
     this.ninjas = this.ninjas.filter((n) => {
       if (n.id === id) toBeRemoved = n;
