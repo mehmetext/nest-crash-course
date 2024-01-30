7c1382d8930f1441279471219d1704ef0bef0790
Mehmet <mehmetext68@gmail.com>
2024-01-30 17:00:40 +0300

    add swagger codes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,23 +2,26 @@ import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @ApiOkResponse({ type: User, isArray: true })
   @Get()
   getUsers(): User[] {
     return this.usersService.findAll();
   }
 
+  @ApiOkResponse({ type: User })
   @Get(':id')
   getUser(@Param('id') id: string): User {
     return this.usersService.findById(+id);
   }
 
+  @ApiCreatedResponse({ type: User })
   @Post()
   createUser(@Body() createUserDto: CreateUserDto): User {
     return this.usersService.createUser(createUserDto);
