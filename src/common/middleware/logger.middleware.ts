b79d3d06f6831fc4fb884cd08fc0cc7b65e98fae
Mehmet <mehmetext68@gmail.com>
2025-07-02 17:20:45 +0300

    remove colors in logger.middleware

--- src/common/middleware/logger.middleware.ts
+++ src/common/middleware/logger.middleware.ts
@@ -9,18 +9,10 @@ export class LoggerMiddleware implements NestMiddleware {
 
     const method = `${'\x1b[1m'}${req.method}${'\x1b[0m'}`;
 
-    let statusCode: string;
-
-    if (res.statusCode >= 200 && res.statusCode < 300) {
-      statusCode = `\x1b[32m${res.statusCode}\x1b[0m`;
-    } else {
-      statusCode = `\x1b[31m${res.statusCode}\x1b[0m`;
-    }
-
     res.on('finish', () => {
       const duration = performance.now() - start;
       console.log(
-        `${method} ➜ ${reqUrl} | ${statusCode} | ${duration.toFixed(2)}ms`,
+        `${method} ➜ ${reqUrl} | ${res.statusCode} | ${duration.toFixed(2)}ms`,
       );
     });
 
