59c7b1ad4a493c9ebd9742b809ae0f7fab06851c
Mehmet <mehmetext68@gmail.com>
2025-07-05 15:39:52 +0300

    update throttler

--- src/app.module.ts
+++ src/app.module.ts
@@ -26,7 +26,23 @@ import { UsersModule } from './users/users.module';
     RefreshTokensModule,
     ScheduleModule.forRoot(),
     ThrottlerModule.forRoot({
-      throttlers: [{ ttl: 60000, limit: 10 }],
+      throttlers: [
+        {
+          name: 'short',
+          ttl: 1000,
+          limit: 3,
+        },
+        {
+          name: 'medium',
+          ttl: 10000,
+          limit: 20,
+        },
+        {
+          name: 'long',
+          ttl: 60000,
+          limit: 100,
+        },
+      ],
     }),
   ],
   controllers: [AppController],
